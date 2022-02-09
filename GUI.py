feab0081212ee6357fe4f96e363cf887b40b758e
kchong98 <kchong12@hotmail.com>
2022-02-08 22:54:26 -0500

    completed gui
    - added plot to GUI
    - removed utiltiy_functions.py
    - moved utility functions to GUI class methods
    - start, stop, exit buttons

--- GUI.py
+++ GUI.py
@@ -1,16 +1,84 @@
 import tkinter as tk
-from utility_functions import *
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from threading import Thread, Event
+import serial
+import numpy as np
+from time import sleep
+import sys
 
-class gui(tk.Tk):
-    def __init__(self):
+
+class gui:
+    def __init__(self, root):
         super().__init__()
-        pass
+        self.root = root
+        self.data = np.array([0,0,0,0])
+        self.stop_data = Event()
+        self.var1 = tk.IntVar()
+        c1 = tk.Checkbutton(self.root, text='Generate Data', variable=self.var1, onvalue=1, offvalue=0)
+        c1.grid(row = 1, column=1, columnspan=3)
 
-    def start(self):
-        pass
+        self.fig = plt.figure(figsize = (5,5))
+        self.ax = self.fig.add_axes([0,0.05,1,1])
+        self.plot()
+        self.canvas = FigureCanvasTkAgg(self.fig, master = self.root)
+        self.canvas.get_tk_widget().grid(row = 2, column=1, columnspan = 3)
+        self.canvas.draw()
+
+        startButton = tk.Button(self.root, text = 'Start', command = self.collect)
+        startButton.grid(row = 3, column = 1)
+
+        stopButton = tk.Button(self.root, text = 'Stop', command = self.stop)
+        stopButton.grid(row = 3, column = 2)
+
+        exitButton = tk.Button(self.root, text = 'Exit', command = self.exit)
+        exitButton.grid(row = 3, column = 3)
+
+    def collect(self):
+        if self.var1.get() == 1:
+            self.thread = Thread(target = self.generateData)
+            self.thread.start()
+        else:
+            self.thread = Thread(target = self.acquireData)
+            self.thread.start()
 
     def stop(self):
+        self.stop_data.set()
         pass
 
-    def scanning(self):
-        pass
+    def exit(self):
+        self.stop()             # stopping thread
+        self.root.destroy()     # close GUI
+        sys.exit()              # stop script
+
+    def acquireData(self, com = 'COM5', baud = 9600):
+        while(1):
+            if (self.stop_data.is_set() == False):
+                ser = serial.Serial(com, baudrate = baud)
+                data = serial.readline()
+                ser.close()
+            else:
+                break
+
+    def generateData(self):
+        while(1):
+            if (self.stop_data.is_set() == False):
+                self.data = np.random.randint(0,6, size = (4,))
+                self.plot() 
+                self.canvas.draw()
+            else:
+                break
+            sleep(0.01)
+
+    def plot(self):
+        self.ax.clear()
+        self.ax.bar([1,2,3,4], self.data, tick_label = ['1','2','3','4'], width = 1, edgecolor = 'g', linewidth = 2, color = 'white')
+        self.ax.set_ylim([0,5])
+        self.ax.axhline(0.5, color = 'r', linestyle = '-') 
+        self.ax.get_yaxis().set_visible(False)
+        plt.tight_layout()
+
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from threading import Thread
 from GUI import *
 import serial
+import tkinter as tk
 
 if __name__ == '__main__':
-    pass
+    root = tk.Tk()
+    my_gui = gui(root)
+    root.mainloop()
